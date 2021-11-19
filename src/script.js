7d3538196a57b5f041dac9d292a563810338245d
Iurie <iuriemisinciuc18@gmail.com>
2021-11-19 16:27:09 +0200

    mobile design

--- src/script.js
+++ src/script.js
@@ -1,4 +1,17 @@
+/* Mobile menu */
+let mobBurgher = document.querySelector(".burgher");
+
+mobBurgher.addEventListener("click", function () {
+  console.log("fine");
+  let navMob = document.querySelector(".navigation-mobile");
+  let burgLine = document.querySelector(".burgher-line");
+  mobBurgher.classList.toggle("burgher-x");
+  burgLine.classList.toggle("burgher-line-x");
+  navMob.classList.toggle("navigation-mobile-active");
+});
+
 /* Menu Dropdown */
+
 let dropDown = document.querySelector(".dropdown");
 let dropDownCont = document.querySelector(".dropdown__content");
 let arrow = document.querySelector(".dropdown__title");
@@ -13,16 +26,51 @@ dropDown.addEventListener("mouseleave", () => {
   arrow.classList.toggle("dropdown__title-anim");
 });
 
-/* Mobile menu */
-let mobBurgher = document.querySelector(".burgher");
+$(".language__list").on("click", ".language__item-init", function () {
+  $(this)
+    .closest(".language__list")
+    .children("li:not(.language__item-init)")
+    .toggle();
+  $(".language__list")
+    .children("li:not(.language__item-init)")
+    .css("margin-top", "14px");
+  $(".language").toggleClass("arrow_anim");
+});
 
-mobBurgher.addEventListener("click", function () {
-  console.log("fine");
-  let navMob = document.querySelector(".navigation-mobile");
-  let burgLine = document.querySelector(".burgher-line");
-  mobBurgher.classList.toggle("burgher-x");
-  burgLine.classList.toggle("burgher-line-x");
-  navMob.classList.toggle("navigation-mobile-active");
+var allOptions = $(".language__list").children("li:not(.language__item-init)");
+$(".language__list").on("click", "li:not(.language__item-init)", function () {
+  $(".language").toggleClass("arrow_anim");
+  allOptions.removeClass("selected");
+  $(this).addClass("selected");
+  $(".language__list").children(".language__item-init").html($(this).html());
+  allOptions.toggle();
+});
+
+let acc = document.getElementsByClassName("accordion");
+
+for (let i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    this.classList.toggle("accordion-anim");
+
+    let panel = this.nextElementSibling;
+    console.log(panel);
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+const swiperteam = new Swiper(".swiper-team", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    type: "bullets",
+  },
 });
 
 /* hover btn1 */
@@ -78,43 +126,6 @@ link3.addEventListener("mouseleave", () => {
     link3.style.visibility = "hidden";
   });
 });
-
-$(".language__list").on("click", ".language__item-init", function () {
-  $(this)
-    .closest(".language__list")
-    .children("li:not(.language__item-init)")
-    .toggle();
-  $(".language__list")
-    .children("li:not(.language__item-init)")
-    .css("margin-top", "14px");
-  $(".language").toggleClass("arrow_anim");
-});
-
-var allOptions = $(".language__list").children("li:not(.language__item-init)");
-$(".language__list").on("click", "li:not(.language__item-init)", function () {
-  $(".language").toggleClass("arrow_anim");
-  allOptions.removeClass("selected");
-  $(this).addClass("selected");
-  $(".language__list").children(".language__item-init").html($(this).html());
-  allOptions.toggle();
-});
-
-let acc = document.getElementsByClassName("accordion");
-
-for (let i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function () {
-    this.classList.toggle("active");
-    this.classList.toggle("accordion-anim");
-
-    let panel = this.nextElementSibling;
-    console.log(panel);
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-  });
-}
 
 const swiper1 = new Swiper(".swiper1", {
   pagination: {
